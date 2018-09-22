ea1dbcd26ec7b9eac6cc361037e6934a7cadb61e
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-09-22 22:07:38 +0530

    add users model

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = require('./core/App');
 require('./public/routes/');
+require('./lib/models');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening to port ${PORT}`);
