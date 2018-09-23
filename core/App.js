7f65482acce391813618c95b26776596ae34a986
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-09-23 10:05:02 +0530

    add API rate limiter

--- core/App.js
+++ core/App.js
@@ -4,7 +4,22 @@
  */
 
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+// specify the maximum number of requests server can accept in a given interval of time
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 10,
+    message: {
+        api_status: "stable",
+        version: "1.0",
+        warning: "Too many requests, please try again after 1 minute"
+    }
+});
+
+app.use(limiter);
 
 module.exports = app;
--- public/routes/index.js
+++ public/routes/index.js
@@ -5,23 +5,52 @@
 
 const app = require('../../core/App');
 
+function calcResponseTime(startTimestamp) {
+    if(!startTimestamp) {
+        return null;
+    }
+    return Date.now() - startTimestamp
+}
+
 function routeHandler() {
 
-    app.get('/v1/user/login', (req, res) => {
+    app.post('/v1/user/login', (req, res) => {
 
-        const base64Auth = (req.headers.authorization || '').split(' ')[1] || '';
-        const [login, password] = new Buffer(base64Auth, 'base64').toString().split(':');
+        var startTime = Date.now();
+        var endTime;
 
+        const base64Credentials = req.get('Basic') || null;
+
+        if(!base64Credentials) {
+            res.status(422).json(
+                {
+                    api_status: "stable",
+                    version: "1.0",
+                    response_time: calcResponseTime(startTime) + ' ms',
+                    data: { 
+                        status: "fail",
+                        message: "missing parameters"
+                    }
+                }
+            );
+            return;
+        }
+
+        console.log(req.get('Basic'));
+        console.log(Buffer.from(req.get('Basic'), 'base64').toString());
+
+        
         return res.json({
-            username: login,
-            password: password
+            status: "success"
         });
 
     });
 
     app.get('/', (req, res) => {
+        const base64Credentials = new Buffer('admin:secret').toString('base64');
         res.send({
-            'Greeting': "Welcome to your workspace"
+            'Greeting': "Welcome to your workspace",
+            'base64Creds': base64Credentials
         });
     });
 
