9068456094756e3081a35f7926057c8c5eb029fc
amirsaleem-kn <amir.saleem@kisannetwork.com>
2018-09-23 08:00:08 +0530

    add comments

--- core/App.js
+++ core/App.js
@@ -1,4 +1,10 @@
+/**
+ * @author Amir Saleem
+ * @description This file contains the core logic for express application
+ */
+
 const express = require('express');
+
 const app = express();
 
 module.exports = app;
--- public/routes/index.js
+++ public/routes/index.js
@@ -1,6 +1,23 @@
+/**
+ * @author Amir Saleem
+ * @description 
+ */
+
 const app = require('../../core/App');
 
 function routeHandler() {
+
+    app.get('/v1/user/login', (req, res) => {
+
+        const base64Auth = (req.headers.authorization || '').split(' ')[1] || '';
+        const [login, password] = new Buffer(base64Auth, 'base64').toString().split(':');
+
+        return res.json({
+            username: login,
+            password: password
+        });
+
+    });
 
     app.get('/', (req, res) => {
         res.send({
